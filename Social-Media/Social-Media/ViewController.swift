532397ce9f61ed0732cdfff16573559693ed2b76
Augustine Ubah <augustine_ubah@yahoo.com>
2019-08-05 21:49:33 -0400

    Initial start to the app

---
+++ Social-Media/Social-Media/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social-Media
+//
+//  Created by Augustine Ubah on 8/5/19.
+//  Copyright © 2019 Augustine Ubah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
